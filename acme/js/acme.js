b1826099987cc9f07fc2d40622330c3bd7dcc066
Aaron Ferguson <aaronferguson_92@yahoo.com>
2017-03-30 14:25:03 -0600

    acme site

--- acme/js/acme.js
+++ acme/js/acme.js
@@ -1,41 +1,68 @@
-function getData(input) {
-    // Get the data from the wunderground API
+$.ajax({
+    url: "/acme/js/acme.json",
+    dataType: "json",
+    success: function (data) {
+        console.log(data);
+
+        var output = '';
+        $.each(data, function (key, val) {
+            output += '<li>';
+            output += '<a href = " ' + this.path + ' ">' + key + '</a>';
+            output += '</li>';
+
+        });
+        console.log(output);
+        $("#nav").append(output);
+    }
+
+});
+$("#nav").on("click", "a", function (evt) {
+    evt.preventDefault();
+
+    var jsonAcme = $(this).text();
+    console.log(each);
     $.ajax({
-        url: "acme.json",
+        url: "/acme/js/acme.json",
         dataType: "json",
         success: function (data) {
             console.log(data);
+            console.log(data[jsonAcme])
+
+
+
             data.Anvils.name;
             data.Explosives.name;
             data.Decoys.name;
             data.Traps.name;
 
-//            $("#anvils").text(data.Anvils.path);
-//            $("#anvils").text(data.Anvils.description);
-//            $("#explosives").text(data.Explosives.path);
-//            $("#explosives").text(data.Explosives.description);
-//            $("#decoys").text(data.Decoys.path);
-//            $("#decoys").text(data.Decoys.description);
-//            $("#traps").text(data.Traps.path);
-//            $("#traps").text(data.Traps.description);
-//            $("#cover").fadeOut(250);
+            $("#anvils").text(data.Anvils.path);
+            $("#anvils").text(data.Anvils.description);
+            $("#anvils").text(data.Anvils.manufacturer);
+            $("#anvils").text(data.Anvils.price);
+            $("#anvils").text(data.Anvils.reviews);
+
+            $("#explosives").text(data.Explosives.path);
+            $("#explosives").text(data.Explosives.description);
+            $("#explosives").text(data.Explosives.manufacturer);
+            $("#explosives").text(data.Explosives.price);
+            $("#explosives").text(data.Explosives.reviews);
+
+
+            $("#decoys").text(data.Decoys.path);
+            $("#decoys").text(data.Decoys.description);
+            $("#decoys").text(data.Decoys.manufacturer);
+            $("#decoys").text(data.Decoys.price);
+            $("#decoys").text(data.Decoys.reviews);
+
+            $("#traps").text(data.Traps.path);
+            $("#traps").text(data.Traps.description);
+            $("#traps").text(data.Traps.manufacturer);
+            $("#traps").text(data.Traps.price);
+            $("#traps").text(data.Traps.reviews);
+            $("#cover").fadeOut(250);
 
 
         }
     });
-}
 
-$("#page").on("click", "a", function (evt) {
-    evt.preventDefault();
-
-    var jsonAcme = $(this).text();
-    console.log(jsonAcme);
-    $.ajax({
-        url: "acme.json"
-        , dataType: "json"
-        , success: function (data) {
-            console.log(data);
-            console.log(data[jsonAcme]);
-            }
-    });
 });
