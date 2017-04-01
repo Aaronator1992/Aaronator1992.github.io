b93f5a17348f9b6b9e170edbf580ce29350abf52
Aaron Ferguson <aaronferguson_92@yahoo.com>
2017-03-31 20:49:30 -0600

    editing

--- acme/js/acme.js
+++ acme/js/acme.js
@@ -2,67 +2,47 @@ $.ajax({
     url: "/acme/js/acme.json",
     dataType: "json",
     success: function (data) {
-        console.log(data);
 
         var output = '';
         $.each(data, function (key, val) {
             output += '<li>';
             output += '<a href = " ' + this.path + ' ">' + key + '</a>';
             output += '</li>';
-
         });
-        console.log(output);
-        $("#nav").append(output);
+        $("nav .flexlist").append(output);
     }
-
 });
-$("#nav").on("click", "a", function (evt) {
-    evt.preventDefault();
-
-    var jsonAcme = $(this).text();
-    console.log(each);
-    $.ajax({
-        url: "/acme/js/acme.json",
-        dataType: "json",
-        success: function (data) {
-            console.log(data);
-            console.log(data[jsonAcme])
 
 
+$(function () {
+    $("nav .flexlist").on("click", ":not(:first-child) a", function (evt) {
+        evt.preventDefault();
 
-            data.Anvils.name;
-            data.Explosives.name;
-            data.Decoys.name;
-            data.Traps.name;
-
-            $("#anvils").text(data.Anvils.path);
-            $("#anvils").text(data.Anvils.description);
-            $("#anvils").text(data.Anvils.manufacturer);
-            $("#anvils").text(data.Anvils.price);
-            $("#anvils").text(data.Anvils.reviews);
-
-            $("#explosives").text(data.Explosives.path);
-            $("#explosives").text(data.Explosives.description);
-            $("#explosives").text(data.Explosives.manufacturer);
-            $("#explosives").text(data.Explosives.price);
-            $("#explosives").text(data.Explosives.reviews);
-
-
-            $("#decoys").text(data.Decoys.path);
-            $("#decoys").text(data.Decoys.description);
-            $("#decoys").text(data.Decoys.manufacturer);
-            $("#decoys").text(data.Decoys.price);
-            $("#decoys").text(data.Decoys.reviews);
-
-            $("#traps").text(data.Traps.path);
-            $("#traps").text(data.Traps.description);
-            $("#traps").text(data.Traps.manufacturer);
-            $("#traps").text(data.Traps.price);
-            $("#traps").text(data.Traps.reviews);
-            $("#cover").fadeOut(250);
+        var jsonAcme = $(this).text();
+        console.log(jsonAcme);
+        $.ajax({
+            url: "/acme/js/acme.json",
+            dataType: "json",
+            success: function (data) {
+                console.log(data);
+                name = data[jsonAcme].name;
+                price = data[jsonAcme].price;
+                description = data[jsonAcme].description;
+                manufacturer = data[jsonAcme].manufacturer;
+                reviews = data[jsonAcme].reviews;
+                product_img = data[jsonAcme].path;
 
 
-        }
+                $('#name').text(name);
+                $('#price').text(price);
+                $('#description').text(description);
+                $('#manufacturer').text('Made by: ' + manufacturer);
+                $('#reviews').text('Rating: ' + reviews + ' Stars');
+
+                $('#product_img').html('<img src="' + product_img + '" alt="'+ name +'">');
+                console.log(price);
+                $('#home').fadeOut();
+            }
+        });
     });
-
 });
